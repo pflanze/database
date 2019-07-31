8545cfc5d65334f1a7a1bce8e679d628566ba8d8
Christian Jaeger <ch@christianjaeger.ch>
2019-07-31 21:07:25 +0100

    Restructure according to standard 'lein new' setup, Eclipse license

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject database "0.0.1"
   :description "A simple object database"
   :url "http://github.com/pflanze/database"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
   ;; :dependencies [[org.clojure/clojure "1.1.0"]
   ;;                [org.clojure/clojure-contrib "1.1.0"]]
   ;; :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
   :dependencies [[mvxcvi/multihash "2.0.3"]]
-  :main database.main)
+  :main database.main
+  :repl-options {:init-ns database.main})
 
