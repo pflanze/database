eefba57179921530ac48fa7d753d73bd3ffab7a0
Christian Jaeger <ch@christianjaeger.ch>
2019-08-27 20:48:15 +0100

    copy of red-black tree from https://github.com/henrygarner/rb-tree
    https://github.com/henrygarner/rb-tree/blob/master/src/rb_tree/core.clj

--- src/database/store.clj
+++ src/database/store.clj
@@ -30,3 +30,63 @@
     ;; XXX security
     (-> (slurp path)
         (read-string))))
+
+
+;; License
+
+;; Copyright © 2016 Henry Garner
+
+;; Distributed under the Eclipse Public License either version 1.0 or (at your option) any later version.
+
+(ns rb-tree.core
+  (:refer-clojure :exclude [assoc contains? keys vals])
+  (:require [clojure.core.match :refer [match]]))
+
+(defn balance [tree]
+  (match [tree]
+    [(:or [:black [:red [:red a x b] y c] z d]
+          [:black [:red a x [:red b y c]] z d]
+          [:black a x [:red [:red b y c] z d]]
+          [:black a x [:red b y [:red c z d]]])] [:red [:black a x b]
+                                                  y
+                                                  [:black c z d]]
+    :else tree))
+
+(defn assoc [tree k v]
+  (let [ins (fn ins [tree]
+              (match tree
+                nil [:red nil (clojure.lang.MapEntry. k v) nil]
+                [color a kv b] (cond
+                                 (< k (key kv)) (balance [color (ins a) kv b])
+                                 (> k (key kv)) (balance [color a kv (ins b)])
+                                 :else tree)))
+        [_ a y b] (ins tree)]
+    [:black a y b]))
+
+(defn contains? [tree k]
+  (match tree
+    nil false
+    [_ a kv b] (cond
+                 (< k (key kv)) (recur a k)
+                 (> k (key kv)) (recur b k)
+                 :else true)))
+
+(defn vals [tree]
+  (match tree
+    nil '()
+    [_ a y b] (concat (vals a) (cons (val y) (vals b)))))
+
+(defn keys [tree]
+  (match tree
+    nil '()
+    [_ a y b] (concat (keys a) (cons (key y) (keys b)))))
+
+;; (defn dissoc [tree x])
+
+;; (defn min-key [tree])
+;; (defn max-key [tree])
+
+;; (defn ceiling-key [tree x])
+;; (defn floor-key [tree x])
+
+;; (defn value-slice [tree min max])
