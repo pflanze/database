da91f52f6df8428efe7e85130c68ca8a2e239117
Christian Jaeger <ch@christianjaeger.ch>
2019-08-27 21:01:36 +0100

    adapt to load

--- src/database/store.clj
+++ src/database/store.clj
@@ -1,3 +1,7 @@
+(ns store
+    (:require [clojure.core.match :refer [match]]))
+
+
 (import 'java.security.MessageDigest
         'java.math.BigInteger)
 
@@ -38,11 +42,9 @@
 
 ;; Distributed under the Eclipse Public License either version 1.0 or (at your option) any later version.
 
-(ns rb-tree.core
-  (:refer-clojure :exclude [assoc contains? keys vals])
-  (:require [clojure.core.match :refer [match]]))
 
-(defn balance [tree]
+
+(defn rb:balance [tree]
   (match [tree]
          [(:or [:black [:red [:red a x b] y c] z d]
                [:black [:red a x [:red b y c]] z d]
@@ -53,20 +55,20 @@
                [:black c z d]]
          :else tree))
 
-(defn assoc [tree k v]
+(defn rb:assoc [tree k v]
   (let [ins
         (fn ins [tree]
             (match tree
                    nil [:red nil (clojure.lang.MapEntry. k v) nil]
                    [color a kv b]
                    (cond
-                    (< k (key kv)) (balance [color (ins a) kv b])
-                    (> k (key kv)) (balance [color a kv (ins b)])
+                    (< k (key kv)) (rb:balance [color (ins a) kv b])
+                    (> k (key kv)) (rb:balance [color a kv (ins b)])
                     :else tree)))
         [_ a y b] (ins tree)]
     [:black a y b]))
 
-(defn contains? [tree k]
+(defn rb:contains? [tree k]
   (match tree
          nil false
          [_ a kv b] (cond
@@ -74,15 +76,17 @@
                      (> k (key kv)) (recur b k)
                      :else true)))
 
-(defn vals [tree]
+(defn rb:vals [tree]
   (match tree
          nil '()
-         [_ a y b] (concat (vals a) (cons (val y) (vals b)))))
+         [_ a y b] (concat (rb:vals a)
+                           (cons (val y) (rb:vals b)))))
 
-(defn keys [tree]
+(defn rb:keys [tree]
   (match tree
          nil '()
-         [_ a y b] (concat (keys a) (cons (key y) (keys b)))))
+         [_ a y b] (concat (rb:keys a)
+                           (cons (key y) (rb:keys b)))))
 
 ;; (defn dissoc [tree x])
 
