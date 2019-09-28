24946f312107e8b97f9bc63337d1c62f8e469306
Christian Jaeger <ch@christianjaeger.ch>
2019-09-28 04:30:18 +0100

    test dbrecord: make clearer

--- test/database/dbrecord.clj
+++ test/database/dbrecord.clj
@@ -1,21 +1,21 @@
 (ns test.database.dbrecord
     (:require [clojure.test :refer :all]
-              [chj.test :refer [is*]]
+              [chj.test :refer [is* is=]]
               [database.store :as s]
               [database.dbrecord :refer [defdbrecord]]))
 
 
 (defdbrecord pair [car cdr])
 
+(deftest constructor
+
+  (is= (Pair. 10 (Pair. 20 30))
+       (pair 10 (pair 20 30))))
+
 (deftest reversibility
 
   (def s (s/open-store "db"))
 
-  (is*
- 
-   (= (->> (Pair. 10 (Pair. 20 30)) (s/store-put s) (s/store-get s))
-      (Pair. 10 (Pair. 20 30)))
-
-   (= (->> (pair 10 (pair 20 30)) (s/store-put s) (s/store-get s))
-      (pair 10 (pair 20 30)))))
+  (is= (->> (pair 10 (pair 20 30)) (s/store-put s) (s/store-get s))
+       (pair 10 (pair 20 30))))
 
